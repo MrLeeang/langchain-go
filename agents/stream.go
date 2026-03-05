397925372efd1bd0bafb59a82f03c819da49c01f
李宏伟 <lihongwei549@163.com>
2026-03-05 15:01:28 +0800

    up

--- agents/stream.go
+++ agents/stream.go
@@ -163,7 +163,7 @@ func (a *Agent) StreamWithContext(ctx context.Context, message string) <-chan St
 
 				if err == context.Canceled {
 					stream.Close()
-					ch <- StreamResponse{Done: true, Error: err}
+					ch <- StreamResponse{Done: true}
 					a.CalculateCompletionTokenUsage(fullContent)
 					return
 				}
